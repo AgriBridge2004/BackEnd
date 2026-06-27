f916e59f7dfca1240e33aa6dc116ccbbd4110d7b
mohammedalaaaila-cloud <mohammed.alaa.aila@gmail.com>
2026-06-27 11:14:43 +0300

    fix it

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -32,8 +32,10 @@ export const verifyRole = (...allowedRoles) => {
       return res.status(401).json({ message: 'User not authenticated' });
     }
 
-   
-  
+    if (req.user.role === 'admin') {
+      return next();
+    }
+
     if (!allowedRoles.includes(req.user.role)) {
       return res.status(403).json({ message: 'Access denied' });
     }
